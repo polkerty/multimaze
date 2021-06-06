a203d089187eec2bb2f631c5dda5f656e47f8027
Beren Gunsolus <gunso010@umn.edu>
2021-06-06 20:52:11 +0000

    Update pages/level.js, pages/levels.json files

--- pages/level.js
+++ pages/level.js
@@ -62,10 +62,7 @@ export default class Level extends Component {
             }
 
             if (this.canAcceptPlayer(newX, newY)) {
-                let playerIndex = this.state.board[x][y].indexOf(TOKEN.PLAYER1);
-                if ( playerIndex > -1 ) {
-                    newContents.splice(playerIndex, 1);
-                }
+
             }
             else {
                    newContents.push(TOKEN.PLAYER1)
