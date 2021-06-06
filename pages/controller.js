be07b12eba88d7eab3e347199b62117e2925a35f
Jacob Brazeal <jacob.brazeal@gmail.com>
2021-06-05 20:17:50 -0400

    First commit

---
+++ pages/controller.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Controller extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <h1>Hello world!</h1>
+    }
+}
