0a12936fd0a7475de681fc72aa86293befcb5246
Jacob Brazeal <jacob.brazeal@gmail.com>
2021-06-05 23:12:12 -0400

    Deugging server-side rendering error

--- pages/controller.js
+++ pages/controller.js
@@ -1,7 +1,79 @@
 import React, {Component} from 'react';
-import {defaultLevels} from './levelConfig';
 import Level from "./level";
-import InputHandler from "./inputHandler";
+
+class InputHandler {
+    constructor() {
+
+        this.handlers = {};
+        if (typeof window === 'undefined') return;
+
+        const self = this;
+        window.addEventListener('keydown', function (e) {
+            switch(e.key) {
+                case 'ArrowLeft':
+                    self.emit('left');
+                    break;
+                case 'ArrowRight':
+                    self.emit('right');
+                    break;
+                case 'ArrowUp':
+                    self.emit('up');
+                    break;
+                case 'ArrowDown':
+                    self.emit('down');
+                    break;
+                case 'r':
+                    self.emit('restart');
+                    break;
+                case 'w':
+                    self.emit('win');
+                    break;
+                default:
+                // Life is good, take it easy
+            }
+        })
+
+    }
+
+    clearAll() {
+        this.handlers = {};
+    }
+
+    emit(event, data = null) {
+        if (this.handlers[event]) {
+            for (const handler of this.handlers[event]) {
+                handler(data)
+            }
+        }
+    }
+
+    on(event, fn) {
+        // Add an event handler
+        this.handlers[event] = this.handlers[event] || [];
+        this.handlers[event].push(fn);
+    }
+
+    off(event, fn) {
+        // Delete the event handler (should be called when level is exited)
+        if (!this.handlers[event]) return;
+        let index = this.handlers[event].indexOf(fn);
+        if (index === -1) return;
+        this.handlers[event].splice(index, 1);
+    }
+
+
+}
+
+class LevelConfig {
+    constructor(id, name, definition) {
+        this.name = name;
+        this.definition = definition;
+    }
+}
+
+const levelDefinitions = require('./levels.json');
+const defaultLevels = levelDefinitions.map(({name, definition}, index) => new LevelConfig(index, name, definition));
+
 
 export default class Controller extends Component {
     constructor(props) {
@@ -21,7 +93,6 @@ export default class Controller extends Component {
     }
 
     onCurrentLevelWin() {
-        console.log("Onward and upward", (this.state.currentLevel + 1 ) % this.state.levels.length, this.state.gameCount + 1);
         this.setState({
             currentLevel: (this.state.currentLevel + 1 ) % this.state.levels.length,
             gameCount: this.state.gameCount + 1
@@ -36,3 +107,4 @@ export default class Controller extends Component {
         </div>)
     }
 }
+
