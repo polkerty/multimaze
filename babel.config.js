0bddd03e6d5c6fcc662bc90e04ca5f24bcc624bb
Jacob Brazeal <jacob.brazeal@gmail.com>
2021-06-05 22:40:36 -0400

    Deugging server-side rendering error

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,5 @@
-const env = require('./env-config.js');
-
 module.exports = {
     babelrc: false,
-    plugins: [
-        ['emotion', { autoLabel: true, sourceMap: true }],
-        'inline-react-svg',
-        ['transform-define', env],
-        ['lodash'],
-    ],
     env: {
         development: {
             presets: ['next/babel'],
@@ -15,18 +7,6 @@ module.exports = {
         },
         production: {
             presets: [['next/babel', { 'preset-env': { targets: { node: true } } }]],
-            plugins: [['emotion', { autoLabel: true, sourceMap: false }]],
-        },
-        test: {
-            presets: [
-                [
-                    'next/babel',
-                    {
-                        'preset-env': { modules: 'commonjs' },
-                        'transform-runtime': { corejs: false }, // https://github.com/zeit/next.js/issues/7050
-                    },
-                ],
-            ],
-        },
-    },
+        }
+    }
 };
