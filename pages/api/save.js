e5524bf1f15943521e1ee145cf2e1639b07ab7f8
Jacob Brazeal <jacob.brazeal@gmail.com>
2021-07-01 21:08:35 -0400

    Make DB save conditional on API being provided

--- pages/api/save.js
+++ pages/api/save.js
@@ -2,20 +2,23 @@
 
 export default async (req, res) => {
 
-  // We've got a payload
+    // We've got a payload
 
-  const payload = JSON.parse(req.body);
+    const payload = JSON.parse(req.body);
 
-  console.log(payload);
+    if (process.env.SAVE_ENDPOINT) {
+        console.log(payload);
+        console.log(process.env.SAVE_ENDPOINT);
+        const submitResults = await fetch(process.env.SAVE_ENDPOINT, {
+            method: 'POST',
+            body: JSON.stringify(payload)
+        }).then(x => x.text())
 
-  console.log(process.env.SAVE_ENDPOINT);
+        console.log("API submit results: ", submitResults);
+    } else {
+        console.log("No save API provided");
+    }
 
-  const submitResults = await fetch(process.env.SAVE_ENDPOINT, {
-    method: 'POST',
-    body: JSON.stringify(payload)
-  }).then(x => x.text())
-
-  console.log("API submit results: ", submitResults);
-  res.statusCode = 200;
-  res.json({ ok: true });
+    res.statusCode = 200;
+    res.json({ok: true});
 }
