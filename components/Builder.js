ce2b10ce588ed22da49959673886a5cc6eae884c
Jacob Brazeal <jacob.brazeal@scale.com>
2024-06-11 22:19:45 -0700

    Add toolbar

--- components/Builder.js
+++ components/Builder.js
@@ -1,82 +1,112 @@
-import React, {Component} from 'react';
-import {faQuestionCircle, faTrophy} from '@fortawesome/free-solid-svg-icons'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import Level from './level'
+import React, { Component } from "react";
+import { faQuestionCircle, faTrophy } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Level, { Cell, TOKEN } from "./level";
 import Leaderboard from "./leaderboard";
-import {Board} from "../utils/logic";
+import { Board } from "../utils/logic";
 import Celebrate from "../utils/celebrate";
-import InputHandler from '../utils/inputHandler';
+import InputHandler from "../utils/inputHandler";
 
 class LevelConfig {
-    constructor(id, name, definition, groups, description) {
-        this.id = id;
-        this.name = name;
-        this.definition = definition;
-        this.groups = groups;
-        this.description = description || '';
-    }
+  constructor(id, name, definition, groups, description) {
+    this.id = id;
+    this.name = name;
+    this.definition = definition;
+    this.groups = groups;
+    this.description = description || "";
+  }
 }
 
 function makeBlankDefinition(rows, cols) {
-    const definition = [];
-    for ( let i = 0; i < rows; ++i ) {
-        const row = [];
-        for ( let j  = 0; j < cols; ++j) {
-            row.push([]);
-        }
-        definition.push(row);
+  const definition = [];
+  for (let i = 0; i < rows; ++i) {
+    const row = [];
+    for (let j = 0; j < cols; ++j) {
+      row.push([]);
     }
-    return definition;
+    definition.push(row);
+  }
+  return definition;
 }
 
-const {puzzles, groups} = require('./levels.json');
-const defaultLevels = puzzles.map(({
-                                       name,
-                                       definition,
-                                       groups,
-                                       description
-                                   }, index) => new LevelConfig(index, name, definition, groups, description));
+const { puzzles, groups } = require("./levels.json");
+const defaultLevels = puzzles.map(
+  ({ name, definition, groups, description }, index) =>
+    new LevelConfig(index, name, definition, groups, description)
+);
 
+export function TokenButton({ type, onClick, active }) {
+  const BASE_CLASS = "builder__tools__token-btn";
+  const ACTIVE = BASE_CLASS + "--active";
+  return (
+    <div tabIndex={-1} className={BASE_CLASS + " " + (active ? ACTIVE : "")}>
+      <Cell def={type ? [type] : []} onClick={onClick} size={30} />
+    </div>
+  );
+}
 export default class Builder extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.inputHandler = new InputHandler();
+    this.inputHandler = new InputHandler();
 
-        const defaultRows = 5, defaultCols = 5;
+    const defaultRows = 5,
+      defaultCols = 5;
 
+    this.state = {
+      //TODO: allow loading levels/groups from lib
+      rows: defaultRows,
+      cols: defaultCols,
+      definition: makeBlankDefinition(defaultRows, defaultCols),
+      activeToken: TOKEN.WALL,
+    };
+  }
 
-        this.state = {
-            //TODO: allow loading levels/groups from lib
-            rows: defaultRows,
-            cols: defaultCols,
-            definition: makeBlankDefinition(defaultRows, defaultCols)
-        }
-
+  componentDidMount() {
+    if (typeof window === "undefined") {
+      return;
     }
+    // TODO: load state from URL (also update URL with state)
+  }
 
-    componentDidMount() {
-        if (typeof window === 'undefined') {
-            return;
-        }
-        // TODO: load state from URL (also update URL with state)
-    }
+  clickHandler(props) {
+    console.log(props.row, props.col, props);
+  }
 
+  render() {
+    return (
+      <div className={"builder-wrap"}>
+        <h1 className={"game-title"}>Create Your Own Level</h1>
 
-    render() {
+        {/* TODO: Allow entering level + description */}
 
-        return (<div className={"level-wrap"}>
-            <h1 className={"game-title"}>Create Your Own Level
-            </h1>
+        <div className="builder">
+          <div className="builder__main-pane">
+            <Level
+              definition={this.state.definition}
+              inputHandler={this.inputHandler}
+              onclick={this.clickHandler.bind(this)}
+            />
+          </div>
+          <div className="builder__tools-pane">
+            <div className="builder__tools__token-picker">
+              {Object.values(TOKEN).map((token) => (
+                <TokenButton
+                  key={token}
+                  active={token === this.state.activeToken}
+                  type={token}
+                  onClick={() => {
+                    this.setState({
+                      activeToken: token,
+                    });
+                  }}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-            { /* TODO: Allow entering level + description */ }
-
-            <Level definition={this.state.definition} inputHandler={this.inputHandler} />
-        </div>)
-    }
-
-    getCurrentGameId() {
-        const level = this.getCurrentLevel();
-        return new Board({grid: level.definition}, {}).getInitialHash();
-    }
 }
--- components/level.js
+++ components/level.js
@@ -1,160 +1,232 @@
-import React, {Component} from "react";
-import {Board} from "../utils/logic";
-import {faUndo, faTimes} from '@fortawesome/free-solid-svg-icons'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import React, { Component } from "react";
+import { Board } from "../utils/logic";
+import { faUndo, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const TOKEN = {
-    WALL: 1,
-    PLAYER1: 2,
-    FINISH1: 3,
-    DEATH: 4,
-    COLLAPSE: 5,
-    BARRIER: 6,
-    COIN: 7,
-    PLAYER2: 8,
-    REVERSER: 9
-}
+  EMPTY: 0, // for convenience
+  WALL: 1,
+  PLAYER1: 2,
+  FINISH1: 3,
+  DEATH: 4,
+  COLLAPSE: 5,
+  BARRIER: 6,
+  COIN: 7,
+  // PLAYER2: 8,
+  // REVERSER: 9
+};
 
-const CODE_TO_TOKEN = Object.fromEntries(Object.entries(TOKEN).map(([a, b]) => [b, a]));
+const CODE_TO_TOKEN = Object.fromEntries(
+  Object.entries(TOKEN).map(([a, b]) => [b, a])
+);
 
 function softDeepCopy(object) {
-    if (!object) return object;
-    return JSON.parse(JSON.stringify(object));
+  if (!object) return object;
+  return JSON.parse(JSON.stringify(object));
 }
 
 export default class Level extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        const grid = softDeepCopy(props.definition || []);
-        this.board = new Board({grid}, {
-            onchange: () => this.onchange(),
-            onwin: (props) => this.win(props),
-            onundo: () => this.onundo(),
-            onrestart: (props) => this.onrestart(props)
-        });
-        this.state = {
-            board: grid,
-            deathByes: [],
-            startTime: new Date().getTime(),
-            deadAnimation: false
-        }
+    const grid = softDeepCopy(props.definition || []);
+    this.board = new Board(
+      { grid },
+      {
+        onchange: () => this.onchange(),
+        onwin: (props) => this.win(props),
+        onundo: () => this.onundo(),
+        onrestart: (props) => this.onrestart(props),
+      }
+    );
+    this.state = {
+      board: grid,
+      deathByes: [],
+      startTime: new Date().getTime(),
+      deadAnimation: false,
+    };
+  }
+
+  onchange() {
+    this.setState(this.board.state);
+  }
+
+  onclick(props) {
+    if (props.onclick) {
+      props.onclick(props);
     }
+  }
 
-    onchange() {
-        this.setState(this.board.state);
+  onrestart(props = {}) {
+    if (props.didDie) {
+      console.log(
+        "It's been such a wonderful friendship... some things will never die."
+      );
+      // Time for an animation
+      this.setState({
+        deadAnimation: true,
+      });
+      setTimeout(
+        () =>
+          this.setState({
+            deadAnimation: false,
+          }),
+        800
+      );
     }
+  }
 
-    onrestart(props = {}) {
-        if (props.didDie) {
+  componentDidMount() {
+    this.board.restart();
 
-            console.log("It's been such a wonderful friendship... some things will never die.")
-            // Time for an animation
-            this.setState({
-                deadAnimation: true
-            })
-            setTimeout(() => this.setState({
-                deadAnimation: false
-            }), 800)
-        }
-    }
+    this.props.inputHandler.clearAll();
+    this.props.inputHandler.on(
+      "swap",
+      () => this.board.availableForMoves() && this.board.swap()
+    );
+    this.props.inputHandler.on(
+      "left",
+      () => this.board.availableForMoves() && this.board.move(0, -1)
+    );
+    this.props.inputHandler.on(
+      "right",
+      () => this.board.availableForMoves() && this.board.move(0, 1)
+    );
+    this.props.inputHandler.on(
+      "up",
+      () =>
+        this.board.availableForMoves() &&
+        this.board.availableForMoves() &&
+        this.board.move(-1, 0)
+    );
+    this.props.inputHandler.on(
+      "down",
+      () => this.board.availableForMoves() && this.board.move(1, 0)
+    );
+    this.props.inputHandler.on(
+      "restart",
+      () => this.board.availableForMoves() && this.board.restart()
+    );
+    this.props.inputHandler.on(
+      "win",
+      () =>
+        this.board.availableForMoves() &&
+        this.win({
+          isSkip: 1,
+        })
+    );
+    this.props.inputHandler.on(
+      "back",
+      () =>
+        this.board.availableForMoves() &&
+        this.win({
+          retreat: 1,
+          isSkip: 1,
+        })
+    );
+    this.props.inputHandler.on("ai", () => this.board.aiSimple());
+    this.props.inputHandler.on(
+      "undo",
+      () => this.board.availableForMoves() && this.board.undo()
+    );
+  }
 
-    componentDidMount() {
-        this.board.restart();
-
-        this.props.inputHandler.clearAll();
-        this.props.inputHandler.on('swap', () => this.board.availableForMoves() && this.board.swap());
-        this.props.inputHandler.on('left', () => this.board.availableForMoves() && this.board.move(0, -1));
-        this.props.inputHandler.on('right', () => this.board.availableForMoves() && this.board.move(0, 1));
-        this.props.inputHandler.on('up', () => this.board.availableForMoves() && this.board.availableForMoves() && this.board.move(-1, 0));
-        this.props.inputHandler.on('down', () => this.board.availableForMoves() && this.board.move(1, 0));
-        this.props.inputHandler.on('restart', () => this.board.availableForMoves() && this.board.restart());
-        this.props.inputHandler.on('win', () => this.board.availableForMoves() && this.win({
-            isSkip: 1
-        }));
-        this.props.inputHandler.on('back', () => this.board.availableForMoves() && this.win({
-            retreat: 1,
-            isSkip: 1
-        }));
-        this.props.inputHandler.on('ai', () => this.board.aiSimple());
-        this.props.inputHandler.on('undo', () => this.board.availableForMoves() && this.board.undo());
-    }
-
-    render() {
-
-        return (<>
-            {this.state.deadAnimation ? <div className={"dead-animation"}/> : ''}
-            <div className={"level-grid"}>
-                {
-                    this.state.board.map((row, index) => (<div key={index} className={"level-row"}>
-                        {
-                            row.map((cell, index) => <Cell key={index} def={cell}/>)
-                        }
-                    </div>))
-                }
+  render() {
+    return (
+      <>
+        {this.state.deadAnimation ? <div className={"dead-animation"} /> : ""}
+        <div className={"level-grid"}>
+          {this.state.board.map((row, rowIndex) => (
+            <div key={rowIndex} className={"level-row"}>
+              {row.map((cell, colIndex) => (
+                <Cell
+                  key={colIndex}
+                  def={cell}
+                  onClick={() => this.onclick({ row: rowIndex, col: colIndex })}
+                />
+              ))}
             </div>
-            <div className={"level-controls"}>
-                {this.board.stack.length ?
-                    <button className={"undo-button"} onClick={() => this.board.undo()}><FontAwesomeIcon icon={faUndo}/>
-                    </button> : ''}
-            </div>
-        </>)
-    }
+          ))}
+        </div>
+        <div className={"level-controls"}>
+          {this.board.stack.length ? (
+            <button className={"undo-button"} onClick={() => this.board.undo()}>
+              <FontAwesomeIcon icon={faUndo} />
+            </button>
+          ) : (
+            ""
+          )}
+        </div>
+      </>
+    );
+  }
 
-    onundo() {
+  onundo() {}
 
-    }
+  win(props = {}) {
+    props = Object.assign(
+      {
+        isSkip: 0,
+        retreat: 0,
+        didCheat: 0,
+        totalMoves: -1,
+        didUndo: 0,
+      },
+      props
+    );
+    this.props.announceVictory?.({
+      gameId: this.board.getInitialHash(),
+      isSkip: props.isSkip,
+      retreat: props.retreat,
+      startTime: this.state.startTime,
+      didCheat: props.didCheat,
+      didUndo: props.didUndo,
+      runTime: (new Date().getTime() - this.state.startTime) / 1000,
+      totalMoves: props.totalMoves,
+    });
+  }
 
-
-    win(props =
-            {}
-    ) {
-        props = Object.assign({
-            isSkip: 0,
-            retreat: 0,
-            didCheat: 0,
-            totalMoves: -1,
-            didUndo: 0
-        }, props);
-        this.props.announceVictory?.({
-            gameId: this.board.getInitialHash(),
-            isSkip: props.isSkip,
-            retreat: props.retreat,
-            startTime: this.state.startTime,
-            didCheat: props.didCheat,
-            didUndo: props.didUndo,
-            runTime: (new Date().getTime() - this.state.startTime) / 1000,
-            totalMoves: props.totalMoves
-        });
-    }
-
-    componentWillUnmount() {
-        this.props.inputHandler.clearAll();
-    }
-
+  componentWillUnmount() {
+    this.props.inputHandler.clearAll();
+  }
 }
 
 export function Cell(props) {
+  const style = {};
+  if (props.size) {
+    style.width = props.size + "px";
+    style.height = props.size + "px";
+  }
 
-    const style = {};
-    if (props.size) {
-        style.width = props.size + 'px';
-        style.height = props.size + 'px';
-    }
+  const fullBodyCell = props.def.find((code) =>
+    [
+      TOKEN.EMPTY,
+      TOKEN.WALL,
+      TOKEN.DEATH,
+      TOKEN.PLAYER1,
+      TOKEN.PLAYER2,
+      TOKEN.FINISH1,
+    ].includes(code)
+  );
 
-    const fullBodyCell = props.def.find(code => [TOKEN.WALL,
-        TOKEN.DEATH, TOKEN.PLAYER1, TOKEN.PLAYER2, TOKEN.FINISH1
-    ].includes(code));
-
-    const cellBackground = fullBodyCell ? 'fill--' + fullBodyCell + ' ' : ' ';
-    return <div style={style}
-                className={cellBackground + 'level-cell' + (props.size ? ' level-cell--sized' : '')}>
-        <div className={"level-cell__spacer"}/>
-        {
-            props.def.map((code, index) => <div key={index}
-                                                className={"level-token level-token--" + CODE_TO_TOKEN[code]}>
-                <div className={"level-cell__spacer"}/>
-            </div>)
-        }
+  const cellBackground = fullBodyCell ? "fill--" + fullBodyCell + " " : " ";
+  return (
+    <div
+      style={style}
+      onClick={props.onClick}
+      className={
+        cellBackground + "level-cell" + (props.size ? " level-cell--sized" : "")
+      }
+    >
+      <div className={"level-cell__spacer"} />
+      {props.def.map((code, index) => (
+        <div
+          key={index}
+          className={"level-token level-token--" + CODE_TO_TOKEN[code]}
+        >
+          <div className={"level-cell__spacer"} />
+        </div>
+      ))}
     </div>
+  );
 }
