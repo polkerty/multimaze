a5e177dfb1367a696f181dbb3988d83a96d29df1
Jacob Brazeal <jacob.brazeal@scale.com>
2024-06-12 23:34:56 -0700

    Untested implenentation of messaging framework for webworkers

--- utils/solveWorker.js
+++ utils/solveWorker.js
@@ -1,3 +1,35 @@
-onmessage = e => {
-    postMessage(Math.random());
+
+// We always just respond to external messages.
+// So we just need to keep track of the ID of the request we're responding to.
+class Controller {
+  constructor() {
+    onmessage = (e) => {
+      const { payload, messageId } = e.data;
+
+      this.handle(payload, messageId);
+    };
+
+  }
+
+  respond(payload, messageId) {
+    postMessage({ payload, messageId })
+  }
+
+  handle(payload, messageId) {
+    let response;
+    switch( payload.action ) {
+        case 'solve':
+            response = this.solve(payload.definition);
+            break;
+        default:
+            response = { error: "Action not recognized: " + payload.action}
+    }
+    this.respond(response, messageId);
+  }
+
+solve(definition) {
+    
+  }
 }
+
+controller = new Controller();
--- utils/workerManager.js
+++ utils/workerManager.js
@@ -6,9 +6,34 @@ class SolveWorkerManager {
         if ( typeof window !== 'undefined' && window.Worker ) {
             this.worker = new SolveWorker();
             this.worker.onmessage = e => {
-                console.log("Message: ", e);
+                const messageId = e.data.messageId;
+                if ( this.messageBuffer[messageId] ) {
+                    this.messageBuffer[messageId].resolve(e.data.payload);
+                } 
+            }
+            this.worker.onmessageerror = e => {
+                const messageId = e.data.messageId;
+                if ( this.messageBuffer[messageId] ) {
+                    
+                } 
             }
         }
+
+        this.messageBuffer = {};
+        
+        this.messageId = 0;
+    }
+
+    async message(payload) {
+        payload.messageId = this.messageId++;
+        const listener = new Promise((res, rej) => {
+            this.messageBuffer[payload.messageId] = { res, rej}
+        })
+        this.worker.postMessage({ payload, messageId });
+    }
+
+    async solve(definition) {
+        await this.message({ action: 'solve', definition })
     }
 }
 
